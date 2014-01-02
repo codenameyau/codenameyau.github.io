9a2954b1686ff6a30b2bd6eb9a8d092af52a28e3
Jorge Yau <jyau01@citymail.cuny.edu>
2014-01-02 10:07:26 -0500

    Adding footer info

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+    $("h2").transition({scale: 1.2, delay: 2000 });
+    $("#start-btn").transition({y: '80px', delay: 5000});
+});
+
+// Transition
+$("h2").hover(
+    function() {
+        $(this).transition({scale: 1.2})},
+    function() {
+        $(this).transition({scale: 1.0})
+    }
+);
+
+$(".service-icon, .lg-icon").hover(
+    function() {
+        $(this).transition({rotate: '45deg'})},
+    function() {
+        $(this).transition({rotate: '0deg'})
+    }
+);
