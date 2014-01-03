8164f91c846770a30d1ca0b84358980f922d9262
Jorge Yau <jyau01@citymail.cuny.edu>
2014-01-02 23:49:11 -0500

    Adding new backgrounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("#email-address").text('codenameyau@gmail.com');
+    $("#email-address").html('codenameyau &#64; gmail.com');
     $("h2").transition({scale: 1.2, delay: 2000 });
     $("#start-btn").transition({y: '80px', delay: 5000});
 });
