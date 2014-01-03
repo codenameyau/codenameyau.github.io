b2e0f9e699f9431c06a8a7b34679d6031482d1e8
Jorge Yau <jyau01@citymail.cuny.edu>
2014-01-03 11:09:25 -0500

    Updating bootstrap gallery

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,29 +1,49 @@
+// Create gallery
 $(document).ready(function() {
+    initGallery('/assets/data/portfolio.json', 3);
+});
+
+function initGallery(jsonFile, rows) {
+
+    // Specify bootstrap column elements
+    var colSize   = 0;
+    var colOffset = 0;
+
+    var newDiv    = {
+        newRow : "<div class='row'><div class='col-md-"+colSize+" col-md-offset-1'>",
+        newCol : "<div class='col-md-"+colSize+"'>"
+    };
 
     // Add items to portfolio
-    $.getJSON('/assets/data/portfolio.json', function(data) {
+    $.getJSON(jsonFile, function(data) {
         var items = [];
         var idCounter = 0;
 
         // Parse JSON to add items to portfolio gallery
         $.each(data.portfolio, function(index, val) {
-            var newDiv = "";
-            // Add a new row
-            if (index%2 == 0)
-                newDiv += "<div class='row'><div class='col-md-4 col-md-offset-2 text-center'>";
-            else
-                newDiv += "<div class='col-md-4 text-center'>";
+            var newImage = "";
 
-            newDiv += "<div class='portfolio-item'><a href='#'><img class='img-portfolio img-thumbnail img-responsive' src='" + val.image + "' data-img='"+ val.image +"' data-title='"+ val.title +"' data-caption='"+ val.caption +"'></a><h4>" + val.title + "</h4></div></div>";
+            // Add a new row
+            if (index%rows == 0)
+                newImage += newDiv.newRow;
+            else
+                newImage += newDiv.newCol;
+
+            // Add new portfolio item
+            newImage += "<div class='portfolio-item'><img class='img-thumbnail img-responsive' src='" + val.image + "' data-img='"+ val.image +"' data-title='"+ val.title +"' data-caption='"+ val.caption +"'><h4>" + val.title + "</h4></div></div>";
 
             // Close new row
-            if (index%2 == 1)
-                newDiv += "</div>"
-            items.push(newDiv);
+            if (index%rows == 1)
+                newImage += "</div>"
+            items.push(newImage);
         });
 
         // Add items to id: portfolio-gallery
         $(items.join("\n")).appendTo("#portfolio-gallery");
-
     });
+}
+
+// Click on thumbnail
+$(document.body).on('click', '.img-thumbnail', function() {
+    alert(0);
 });
